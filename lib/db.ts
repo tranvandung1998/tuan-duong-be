a81b5d06971cc258b6a44244f25ce9e7e670da2e
DungTran <tvdung182898@gmail.com>
2025-08-12 19:10:18 +0700

    remote file

--- lib/db.ts
+++ lib/db.ts
@@ -1,21 +1,10 @@
-// lib/db.ts
-import { Pool } from "pg";
+import { Pool } from 'pg';
 
-declare global {
-  // Thêm khai báo này để TS hiểu global._pgPool là gì
-  // eslint-disable-next-line no-var
-  var _pgPool: Pool | undefined;
-}
-
-let pool: Pool;
-
-if (!global._pgPool) {
-  global._pgPool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-  });
-}
-
-pool = global._pgPool;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export default pool;
