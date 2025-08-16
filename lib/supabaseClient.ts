96d69e545cd7a9f48c87a5a8225841d0ab4dbf4d
DungTran <tvdung182898@gmail.com>
2025-08-16 22:39:15 +0700

    ,,,,

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -4,4 +4,3 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
-
