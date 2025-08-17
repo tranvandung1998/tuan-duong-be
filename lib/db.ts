5f41c78eba217d929a49285176fad597cbbd9981
DungTran <tvdung182898@gmail.com>
2025-08-17 23:41:44 +0700

    ....

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,26 @@
-import { Pool } from 'pg';
+// import { Pool } from 'pg';
 
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production';
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: isProduction
+//     ? { rejectUnauthorized: false }
+//     : false,
+// });
+
+// export default pool;
+
+import pkg from "pg";
+const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: isProduction
-    ? { rejectUnauthorized: false }
-    : false,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 });
 
 export default pool;
+
