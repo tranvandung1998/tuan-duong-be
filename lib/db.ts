f454688ba45a9839a5ea67d877ffa758fbbf1200
DungDev <tvdung182898@gmail.com>
2025-09-16 08:24:36 -0700

    .....

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,11 @@
-import { Pool } from 'pg';
+import pkg from "pg";
+const { Pool } = pkg;
+
+if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is missing");
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-  ssl: {
-    rejectUnauthorized: false, // cần cho Supabase / Railway / Neon
-  },
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // Supabase luôn cần SSL
 });
 
 export default pool;
