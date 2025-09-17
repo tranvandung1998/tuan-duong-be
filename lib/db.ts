1dcdcbed6f4b2612502776ce392a0a19c13f6405
DungDev <tvdung182898@gmail.com>
2025-09-17 18:56:48 +0700

    hhhhhhh

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,10 @@
-import pkg from "pg";
-const { Pool } = pkg;
-
-if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is missing");
+import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, // Supabase luôn cần SSL
+  ssl: {
+    rejectUnauthorized: false, // Supabase yêu cầu SSL
+  },
 });
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 export default pool;
